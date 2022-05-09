dbb3d8b8366fcfdeb491e7ca6ab84dd2ee31b850
ML6503 <ml6503@gmail.com>
2022-05-09 11:59:46 +0300

    feat: add models, route files

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const sequelize = require('./db');
 const models = require('./models/models');
@@ -6,6 +7,12 @@ const models = require('./models/models');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'WORKING!!'})
+});
 
 const start = async () => {
     try {
