5f9b7f16b6abde468f2bbdb18a2737f8b05d88c6
ML6503 <ml6503@gmail.com>
2022-05-15 22:18:09 +0300

    feat: add controllers, middleware, fileupload

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,9 +1,20 @@
-class TypeController {
-    async create(req, res) {
+const { Type } = require("../models/models");
+const ApiError = require('../error/ApiError');
 
+
+class TypeController {
+    async create(req, res, next) {
+        const { name } = req.body;
+        if(!name) {
+            return next(ApiError.badRequest('No type name provided'));
+        }
+        const type = await Type.create({ name });
+        return res.json(type);
     }
 
     async getAll(req, res) {
+        const types = await Type.findAll();
+        return res.json(types);
 
     }
   
