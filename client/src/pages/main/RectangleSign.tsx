0b9fab67a24daec9155e52b9e8e668f02f21d913
ML6503 <ml6503@gmail.com>
2022-12-18 22:39:32 +0200

    feat: add handmade cy text to sign

--- client/src/pages/main/RectangleSign.tsx
+++ client/src/pages/main/RectangleSign.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const RectangleSign = () => {
-  return <div className="rectangle-sign"></div>;
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-end rectangle-sign">
+      <span className="p-3 ">
+        <p className="p-0 m-0 welcome-txt">HANDMADE</p>
+        <p className="p-0 welcome-txt">CYPRUS</p>
+      </span>
+    </div>
+  );
 };
 
 export default RectangleSign;
