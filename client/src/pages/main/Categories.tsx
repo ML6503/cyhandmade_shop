eef37863bee9a66d6169da35ffe05ca9e15b4b65
ML6503 <ml6503@gmail.com>
2022-12-25 23:07:16 +0200

    feat: add knife icon to active category list item

--- client/src/pages/main/Categories.tsx
+++ client/src/pages/main/Categories.tsx
@@ -1,6 +1,8 @@
 import { ShopContext } from 'index';
 import { observer } from 'mobx-react-lite';
-import React, { Dispatch, FC, SetStateAction, useContext } from 'react';
+import React, { Dispatch, FC, SetStateAction, useContext, useState } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { KnifeIcon } from './KnifeIcon';
 
 interface CategoriesProps {
   setCategoryName: Dispatch<SetStateAction<string>>;
@@ -9,29 +11,33 @@ interface CategoriesProps {
 const Categories: FC<CategoriesProps> = observer(({ setCategoryName }) => {
   const appContext = useContext(ShopContext);
   const items = appContext!.items;
+  const [activeCatId, setActiveCatId] = useState(0);
 
-  const addActiveCategory = (e: React.MouseEvent, catName: string) => {
+  const addActiveCategory = (e: React.MouseEvent, catName: string, i: number) => {
     e.preventDefault();
-
+    setActiveCatId(i);
     setCategoryName(catName);
   };
 
   return (
-    <ul className="pb-4 categories-filter list-unstyled">
+    <ListGroup className="pb-4 categories-filter list-unstyled">
       <h6 className="text-uppercase fw-bold">categories</h6>
-      {items.types.map((cat) => {
+      {items.types.map((cat, i) => {
         const catName: string = cat.name;
+        console.log('cat.id', cat.id);
+        console.log('selectedTypeId', items.selectedType.id);
         return (
           <li
             key={cat.id}
-            className={cat.id === items.selectedType.id ? 'lh-sm m-0 cat-active' : 'lh-sm m-0'}
-            onClick={(e) => addActiveCategory(e, catName)}
+            className={i === activeCatId ? 'lh-sm m-0 cat-active' : 'lh-sm m-0'}
+            onClick={(e) => addActiveCategory(e, catName, i)}
           >
+            {i === activeCatId && <KnifeIcon />}
             {catName}
           </li>
         );
       })}
-    </ul>
+    </ListGroup>
   );
 });
 
