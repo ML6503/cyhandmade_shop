705ba25541806a7d70662db948837af7812a764a
ML6503 <ml6503@gmail.com>
2023-02-18 22:41:01 +0200

    feat: add login

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Card, Container, Form } from 'react-bootstrap';
+import { Button, Card, Container, Form } from 'react-bootstrap';
 
 const Auth = () => {
   return (
-    <Container className="auth-container d-flex justify-content-center align-items-center ">
-      <Card>
-        <Form></Form>
-      </Card>
+    <Container className="auth-container d-flex flex-column justify-content-center align-items-center ">
+      <div className="auth-card-wrapper">
+        <h1 className="auth-header">Login</h1>
+        <Card className="auth-card p-5">
+          <Form className="h-100 d-flex flex-column justify-content-between align-items-center">
+            <Form.Control className="input-auth" placeholder="Please enter your email..." />
+            <Form.Control className="input-auth" placeholder="Please enter your password..." />
+            <div className="h-50 w-100 d-flex justify-content-between align-items-center">
+              <div className="w-50 d-flex flex-column justify-content-between create-account">
+                <span className="d-flex">
+                  <p>No account yet?</p>
+                  <a className="create-account-link">Create account</a>
+                </span>
+                <div className="d-flex">
+                  <Form.Check className="check-remember" id="check-remember" />
+                  <Form.Check.Label className="align-middle text-center" htmlFor="check-remember">
+                    Remember me
+                  </Form.Check.Label>
+                </div>
+              </div>
+              <div className="d-flex flex-column justify-content-between">
+                <Button className="auth-btn ">Log in</Button>
+                <a className="lost-password pt-3">Lost your password?</a>
+              </div>
+            </div>
+          </Form>
+        </Card>
+      </div>
     </Container>
   );
 };
