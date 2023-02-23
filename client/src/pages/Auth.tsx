c2bc4530410fd92c9aedcc7f97f2faf78b78e72c
ML6503 <ml6503@gmail.com>
2023-02-23 22:49:53 +0200

    feat: add create account form

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,34 +1,63 @@
 import React from 'react';
 import { Button, Card, Container, Form } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, PRIVACY_POLICY, REGISTRATION_ROUTE } from 'utils/constants';
 
 const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname !== `/${LOGIN_ROUTE}`;
+
   return (
     <Container className="auth-container d-flex flex-column justify-content-center align-items-center ">
       <div className="auth-card-wrapper">
-        <h1 className="auth-header">Login</h1>
-        <Card className="auth-card p-5">
-          <Form className="h-100 d-flex flex-column justify-content-between align-items-center">
+        <h1 className="auth-header">{isLogin ? 'Login' : 'Create Account'}</h1>
+        <Card className="auth-card px-5 pt-4">
+          <Form className="h-100 py-4 d-flex flex-column justify-content-between align-items-center">
             <Form.Control className="input-auth" placeholder="Please enter your email..." />
+            <div className="mb-3"></div>
             <Form.Control className="input-auth" placeholder="Please enter your password..." />
+            <div className="mb-3"></div>
             <div className="h-50 w-100 d-flex justify-content-between align-items-center">
               <div className="w-50 d-flex flex-column justify-content-between create-account">
                 <span className="d-flex">
-                  <p>No account yet?</p>
-                  <a className="create-account-link">Create account</a>
+                  <p>{isLogin ? 'No account yet?' : 'Do you have an account?'}</p>
+                  {isLogin ? (
+                    <NavLink to={REGISTRATION_ROUTE} className="create-account-link">
+                      Create account
+                    </NavLink>
+                  ) : (
+                    <NavLink to={LOGIN_ROUTE} className="create-account-link ">
+                      Login
+                    </NavLink>
+                  )}
                 </span>
                 <div className="d-flex">
-                  <Form.Check className="check-remember" id="check-remember" />
-                  <Form.Check.Label className="align-middle text-center" htmlFor="check-remember">
+                  {/* TODO Remember me logic */}
+                  {/* <Form.Check className="check-remember" id="check-remember" /> */}
+                  {/* <Form.Check.Label className="align-middle text-center" htmlFor="check-remember">
                     Remember me
-                  </Form.Check.Label>
+                  </Form.Check.Label> */}
                 </div>
               </div>
               <div className="d-flex flex-column justify-content-between">
-                <Button className="auth-btn ">Log in</Button>
-                <a className="lost-password pt-3">Lost your password?</a>
+                {isLogin ? (
+                  <>
+                    <Button className="auth-btn">Log in</Button>
+                    <a className="lost-password pt-3">Lost your password?</a>
+                  </>
+                ) : (
+                  <Button className="auth-btn create-acc-btn">Create Account</Button>
+                )}
               </div>
             </div>
           </Form>
+          {!isLogin && (
+            <>
+              <p className="policy-text">
+                {PRIVACY_POLICY} <a>privacy policy.</a>
+              </p>
+            </>
+          )}
         </Card>
       </div>
     </Container>
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -7,3 +7,8 @@ export const LOGIN_ROUTE = 'login';
 export const REGISTRATION_ROUTE = 'registraion';
 export const PRODUCTS_ROUTE = 'products';
 export const CONTACT_ROUTE = 'contact';
+
+export const PRIVACY_POLICY = `
+Your personal data will be used to support your experience
+throughout this website, to manage access to your account,
+and for other purposes described in our`;
